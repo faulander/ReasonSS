82ebbd5719ea0ed97a801e830e3eec02236feefb
Harald Fauland <harald.fauland@gmail.com>
2018-05-15 18:31:15 +0200

    Completed in reverse order

--- main.py
+++ main.py
@@ -37,10 +37,8 @@ logger.addHandler(loghandler)
 
 def mid(s, offset, amount):
     return s[offset-1:offset+amount-1]
-
 def left(s, amount):
     return s[:amount]
-
 def messaging(title,body):
     apobj = apprise.Apprise()
     if config['Notification']['Pushover']:
@@ -49,6 +47,61 @@ def messaging(title,body):
             title=title,
             body=body,
         )
+def cfgfile():
+    if len(config.read('config.txt')) == 0:  # if config file isn't found, create it
+        cfgfile = open("config.txt", 'w')
+        config.add_section('Default')
+        config.add_section('Rss')
+        config.add_section('Torrent')
+        config.add_section('Notification')
+        config.set('Default', 'wanted', 'wanted.txt')  # Every Searchterm in a line in the wanted file
+        config.set('Default', 'completed', 'completed.txt')  # Torrents which have been sent to qBittorrent
+        config.set('Default', 'purge', '30')  # How old downloads should be monitored? (in days)
+        config.set('Notification', '#Pushover', 'pover://user@token')  # see more @ https://github.com/caronc/apprise
+        ###other configs will follow
+        config.set('Notification', '#Boxcar', 'boxcar://hostname')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Discord',
+                   'discord://webhook_id/webhook_token')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Emby', 'emby://user@hostname/')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Boxcar', 'boxcar://hostname')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Faast',
+                   'faast://authorizationtoken')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Growl',
+                   'growl://password@hostname:port')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#IFTT',
+                   'ifttt://webhooksID/EventToTrigger')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Join', 'join://apikey/device')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#KODI', 'kodi://hostname')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Mattermost',
+                   'mmost://hostname/authkey')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Prowl', 'prowl://apikey')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Pushalot',
+                   'palot://authorizationtoken')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#PushBullett', 'pbul://accesstoken')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Pushjet', 'pjet://secret')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Rocketchat',
+                   'rocket://user:password@hostname/RoomID/Channel')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Slack',
+                   'slack://TokenA/TokenB/TokenC/Channel')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Stride',
+                   'stride://auth_token/cloud_id/convo_id')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Telegram',
+                   'tgram://bottoken/ChatID')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#Twitter',
+                   'tweet://user@CKey/CSecret/AKey/ASecret')  # see more @ https://github.com/caronc/apprise
+        config.set('Notification', '#mailto://',
+                   'mailto://userid:pass@domain.com')  # see more @ https://github.com/caronc/apprise
+
+        config.set('Rss', 'rss', 'http://www.example.org/feed')  # Feed Adress
+        config.set('Torrent', 'qbclient', 'http://127.0.0.1:8080')  # WEB-Access for qBittorrent must be available
+        config.set('Torrent', 'category', '')  # no category as standard
+        config.set('Torrent', 'login', 'admin')  # Login to qBittorrent Client
+        config.set('Torrent', 'password', 'admin')  # Password to qBittorrent Client
+        config.set('Torrent', 'download', 'no')  # Start Downloads immediately (yes/no)
+
+        config.write(cfgfile)
+        cfgfile.close()
+
 
 wanted = list()
 completed = list()
@@ -57,52 +110,12 @@ countWanted = int()
 countCompleted = int()
 Downloadlist = list()
 newdownloads = int()
-logger.info('---------------------------------------------------------------------------------------')
 
+logger.info('---------------------------------------------------------------------------------------')
 logger.info('Run at: %s', pendulum.now())
 
 config = configparser.ConfigParser()
-if len(config.read('config.txt'))==0:                           #if config file isn't found, create it
-    cfgfile = open("config.txt", 'w')
-    config.add_section('Default')
-    config.add_section('Rss')
-    config.add_section('Torrent')
-    config.add_section('Notification')
-    config.set('Default','wanted', 'wanted.txt')                #Every Searchterm in a line in the wanted file
-    config.set('Default','completed', 'completed.txt')          #Torrents which have been sent to qBittorrent
-    config.set('Default', 'purge', '30')                        #How old downloads should be monitored? (in days)
-    config.set('Notification', '#Pushover', 'pover://user@token')#see more @ https://github.com/caronc/apprise
-                                                                ###other configs will follow
-    config.set('Notification', '#Boxcar', 'boxcar://hostname')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Discord', 'discord://webhook_id/webhook_token')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Emby', 'emby://user@hostname/')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Boxcar', 'boxcar://hostname')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Faast', 'faast://authorizationtoken')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Growl', 'growl://password@hostname:port')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#IFTT', 'ifttt://webhooksID/EventToTrigger')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Join', 'join://apikey/device')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#KODI', 'kodi://hostname')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Mattermost', 'mmost://hostname/authkey')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Prowl', 'prowl://apikey')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Pushalot', 'palot://authorizationtoken')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#PushBullett', 'pbul://accesstoken')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Pushjet', 'pjet://secret')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Rocketchat', 'rocket://user:password@hostname/RoomID/Channel')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Slack', 'slack://TokenA/TokenB/TokenC/Channel')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Stride', 'stride://auth_token/cloud_id/convo_id')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Telegram', 'tgram://bottoken/ChatID')  #see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#Twitter', 'tweet://user@CKey/CSecret/AKey/ASecret')  # see more @ https://github.com/caronc/apprise
-    config.set('Notification', '#mailto://', 'mailto://userid:pass@domain.com')  # see more @ https://github.com/caronc/apprise
-
-    config.set('Rss','rss', 'http://www.example.org/feed')      #Feed Adress
-    config.set('Torrent','qbclient', 'http://127.0.0.1:8080')   #WEB-Access for qBittorrent must be available
-    config.set('Torrent','category','')                         #no category as standard
-    config.set('Torrent','login','admin')                       #Login to qBittorrent Client
-    config.set('Torrent','password','admin')                    #Password to qBittorrent Client
-    config.set('Torrent','download','no')                       #Start Downloads immediately (yes/no)
-
-    config.write(cfgfile)
-    cfgfile.close()
+cfgfile()
 
 try:
     qb=Client(config['Torrent']['qbclient'])
